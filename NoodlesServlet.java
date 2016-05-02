b6934aa16c1ce2fa23ddf09c7425438048e73ba0
Moyiyang <m0418yy@163.com>
2016-05-02 19:51:18 +0800

    add a class

--- NoodlesServlet.java
+++ NoodlesServlet.java
@@ -8,3 +8,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+public class NoodlesServlet extends HttpServlet {
+}
